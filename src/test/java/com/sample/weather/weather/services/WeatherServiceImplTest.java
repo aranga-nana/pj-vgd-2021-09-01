27b581c8b81e3d95b16de689502a6ac9e180ca5b
arangananayakkara <arangananayakkara@fintrix.com.au>
2021-09-04 18:18:27 +1000

    Weather service test completed

--- src/test/java/com/sample/weather/weather/services/WeatherServiceImplTest.java
+++ src/test/java/com/sample/weather/weather/services/WeatherServiceImplTest.java
@@ -2,6 +2,7 @@ package com.sample.weather.weather.services;
 
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -12,6 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
+import com.sample.weather.weather.common.ErrorCode;
 import com.sample.weather.weather.common.OutputResult;
 import com.sample.weather.weather.domain.Weather;
 import com.sample.weather.weather.repository.WeatherRepository;
@@ -24,6 +26,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Calendar;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -69,17 +72,50 @@ class WeatherServiceImplTest {
     }
     @Test
     @DisplayName("getWeather: from the api service (not found in db) - SUCCESS")
-    void getWeatherFromApi() {
-        Weather mocWeather = new Weather();
-        mocWeather.setCity("Melbourne");
-        mocWeather.setCountry("Australia");
-        mocWeather.setDescription("Sunny");
+    void getWeatherFromApiNotFoundInDb() {
+
         doReturn(Optional.empty()).when(repository).findByCountryAndCity("Australia", "Sydney");
         doReturn(Optional.of("Sunny")).when(apiService).getWeatherDescription("Australia", "Sydney");
 
         OutputResult<WeatherInfoDTO> found = weatherService.getWeather("Australia","Sydney");
         assertTrue(found.isSuccess(),"GET WEATHER - FROM DB ");
         assertEquals("Sunny",found.getData().getDescription());
+    }
+    @Test
+    @DisplayName("getWeather: from the api service (ttl of weather records expires) - SUCCESS")
+    void getWeatherFromApiTTLExpired() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.HOUR,-6);
+        Weather mocWeather = new Weather();
+        mocWeather.setCity("Melbourne");
+        mocWeather.setCountry("Australia");
+        mocWeather.setDescription("Sunny");
+        mocWeather.setUpdated(calendar.getTime()); // expired
+
+        doReturn(mocWeather).when(repository).findByCountryAndCity("Australia", "Sydney");
+        doReturn(Optional.of("Sunny")).when(apiService).getWeatherDescription("Australia", "Sydney");
+
+        OutputResult<WeatherInfoDTO> found = weatherService.getWeather("Australia","Sydney");
+        assertTrue(found.isSuccess());
+        assertEquals("Sunny",found.getData().getDescription());
+    }
+    @Test
+    @DisplayName("getWeather: from the api service with error - FAILED")
+    void getWeatherApiError() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.HOUR,-6);
+        Weather mocWeather = new Weather();
+        mocWeather.setCity("Melbourne");
+        mocWeather.setCountry("Australia");
+        mocWeather.setDescription("Sunny");
+        mocWeather.setUpdated(calendar.getTime()); // expired
+
+        doReturn(mocWeather).when(repository).findByCountryAndCity("Australia", "Sydney");
+        doThrow(new IllegalStateException("Invalid api key")).when(apiService).getWeatherDescription("Australia", "Sydney");
+
+        OutputResult<WeatherInfoDTO> found = weatherService.getWeather("Australia","Sydney");
+        assertFalse(found.isSuccess());
+        assertEquals(ErrorCode.internalServiceError,found.getErrorCode());
     }
 
 }
